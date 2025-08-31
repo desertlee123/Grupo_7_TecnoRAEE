ef2d9b6e3aa8dcbae542ac7f69cf3f044ad8483f
desertlee123 <nahueld081@gmail.com>
2025-08-31 20:31:57 -0300

    innit

---
+++ src/main/java/ar/edu/unlpam/ing/ProyectoAyDSII/ProyectoAyDsiiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.ProyectoAyDSII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAyDsiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAyDsiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlpam/ing/ProyectoAyDSII/ProyectoAyDsiiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.ProyectoAyDSII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAyDsiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
